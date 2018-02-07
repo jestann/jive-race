8439c5cfbb99698d21f24149aa40aa5f1c229b85
Jess Bird <jessannbird@gmail.com>
2018-02-07 20:53:12 +0000

    building reducer

--- src/index.js
+++ src/index.js
@@ -1,44 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom' // includes { render } from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css';
+import { createStore } from 'redux'
 import registerServiceWorker from './tools/registerServiceWorker'
 
-import './css/main/main.css'
 import App from './components/app'
+import reducer from './store/reducers/reducer'
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/main/main.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-registerServiceWorker()
-
-/*
-// scaffolding for redux
-
-import { createStore } from 'redux'
-
-// redux
-import reducer from './store/reducers'
 let store = createStore(reducer)
 
-// styles
-// import 'bootstrap/dist/css/bootstrap.css';
-// import registerServiceWorker from './tools/registerServiceWorker'
-
-// react
-import 'main.css'
-import App from './components/app'
-
-function render () {
+const render = () => {
   ReactDOM.render(
-    <div className="index">
-      <App store={store}/> 
-    </div>,
+    <App store={store} />, 
     document.getElementById('root')
   )
 }
 
 store.subscribe(render)
 render()
-// registerServiceWorker()
+registerServiceWorker()
 
 // can add additional components in div with same store, like <Results store={store}/>
-*/
