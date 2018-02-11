5cacdc9098cff4ed9572dd6b2521d0461cb38773
Jess Bird <jessannbird@gmail.com>
2018-02-11 18:44:16 +0000

    scaffolding reducer integration

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom' // includes { render } from 'react-dom'
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
 import registerServiceWorker from './tools/registerServiceWorker'
 
 import App from './components/app'
-import reducer from './store/reducers/reducer'
+// import reducer from './store/reducers/reducer'
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/main/main.css'
 
-let store = createStore(reducer)
+// let store = createStore(reducer)
 
 const render = () => {
   ReactDOM.render(
-    <App store={store} />, 
+    <App /* store={store} */ />, 
     document.getElementById('root')
   )
 }
 
-store.subscribe(render)
-render()
+// store.subscribe(render)
+// render()
 registerServiceWorker()
 
 // can add additional components in div with same store, like <Results store={store}/>
