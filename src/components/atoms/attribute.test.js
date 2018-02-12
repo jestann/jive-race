58ee622bec489014ffd44a629303fb8d27871acc
Jess Bird <jessannbird@gmail.com>
2018-02-12 01:02:17 +0000

    scaffoling unit tests

---
+++ src/components/atoms/attribute.test.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import Attribute from './../components/atoms/attribute'
+
+describe('general attribute', () => {
+	it('renders without crashing', () => {
+	  shallow(<Attribute />)
+	})
+	
+	it('receives all its props', () => {
+		let wrapper = shallow(<Attribute content='test' />)
+		expect(wrapper.props().test).toEqual('test')
+	})
+	
+	it('renders its content prop', () => {
+		let wrapper = shallow(<Attribute content='test' />)
+		let content = <div className='attribute-short'>test</div>
+		expect(wrapper.contains(content)).toEqual(true)
+	})
+})
+
+describe ('attribute in an attribute list', () => {
+	let wrapper = shallow(<Attribute content='test' link='link' keyed='1' />)
+	
+	it('renders a content div', () => {
+		
+	})
+	
+	it('')
+})
+
+describe ('solo attribute with a link and a postlink', () => {
+	let wrapper = shallow(<Attribute label='label' content='test' link='link' postLink='postlink' />)
+	it('renders the label', () => {
+		
+	})
+})
+
+// TEST
+
+// what it renders + what it passes to them
+// props it receives + what does with them
+// state it holds + when updates
+// user input + callbacks
+// context
+// life cycle side effects: what happens when mounted/unmounted?
+
+/* 
+// EXAMPLES
+
+it('renders three <Foo /> components', () => {
+  const wrapper = shallow(<MyComponent />);
+  expect(wrapper.find(Foo)).to.have.length(3);
+});
+
+it('renders an `.icon-star`', () => {
+  const wrapper = shallow(<MyComponent />);
+  expect(wrapper.find('.icon-star')).to.have.length(1);
+});
+
+it('renders children when passed in', () => {
+  const wrapper = shallow((
+    <MyComponent>
+      <div className="unique" />
+    </MyComponent>
+  ));
+  expect(wrapper.contains(<div className="unique" />)).to.equal(true);
+});
+
+it('simulates click events', () => {
+  const onButtonClick = sinon.spy();
+  const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
+  wrapper.find('button').simulate('click');
+  expect(onButtonClick).to.have.property('callCount', 1);
+});
+
+it('allows us to set props', () => {
+  const wrapper = mount(<Foo bar="baz" />);
+  expect(wrapper.props().bar).to.equal('baz');
+  wrapper.setProps({ bar: 'foo' });
+  expect(wrapper.props().bar).to.equal('foo');
+});
+
+it('simulates click events', () => {
+  const onButtonClick = sinon.spy();
+  const wrapper = mount((
+    <Foo onButtonClick={onButtonClick} />
+  ));
+  wrapper.find('button').simulate('click');
+  expect(onButtonClick).to.have.property('callCount', 1);
+});
+
+it('calls componentDidMount', () => {
+  sinon.spy(Foo.prototype, 'componentDidMount');
+  const wrapper = mount(<Foo />);
+  expect(Foo.prototype.componentDidMount).to.have.property('callCount', 1);
+  Foo.prototype.componentDidMount.restore();
+});
+
+*/
--- src/spec/app.test.js
+++ src/spec/app.test.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { expect } from 'jest' -- throws an error
 import { shallow } from 'enzyme'
 import App from './../components/app'
 
@@ -7,23 +6,109 @@ it('renders without crashing', () => {
   shallow(<App />)
 })
 
+/*
+it("always renders a div", () => {
+  const divs = lockScreen().find("div");
+  expect(divs.length).toBeGreaterThan(0);
+});
+
+let props;
+  let mountedLockScreen;
+  const lockScreen = () => {
+    if (!mountedLockScreen) {
+      mountedLockScreen = mount(
+        <LockScreen {...props} />
+      );
+    }
+    return mountedLockScreen;
+  }
+
+  beforeEach(() => {
+    props = {
+      wallpaperPath: undefined,
+      userInfoMessage: undefined,
+      onUnlocked: undefined,
+    };
+    mountedLockScreen = undefined;
+  });
+
+it("always renders a div", () => {
+  const divs = lockScreen().find("div");
+  expect(divs.length).toBeGreaterThan(0);
+});
+
+describe("the rendered div", () => {
+  it("contains everything else that gets rendered", () => {
+    const divs = lockScreen().find("div");
+    // When using .find, enzyme arranges the nodes in order such
+    // that the outermost node is first in the list. So we can
+    // use .first() to get the outermost div.
+    const wrappingDiv = divs.first();
+
+    // Enzyme omits the outermost node when using the .children()
+    // method on lockScreen(). This is annoying, but we can use it
+    // to verify that wrappingDiv contains everything else this
+    // component renders.
+    expect(wrappingDiv.children()).toEqual(lockScreen().children());
+  });
+});
+
+it("always renders a `ClockDisplay`", () => {
+  expect(lockScreen().find(ClockDisplay).length).toBe(1);
+});
+
+describe("rendered `ClockDisplay`", () => {
+  it("does not receive any props", () => {
+    const clockDisplay = lockScreen().find(ClockDisplay);
+    expect(Object.keys(clockDisplay.props()).length).toBe(0);
+  });
+});
+
+describe("when `onUnlocked` is defined", () => {
+  beforeEach(() => {
+    props.onUnlocked = jest.fn();
+  });
+
+  it("sets the rendered `SlideToUnlock`'s `onSlide` prop to the same value as `onUnlocked`'", () => {
+    const slideToUnlock = lockScreen().find(SlideToUnlock);
+    expect(slideToUnlock.props().onSlide).toBe(props.onUnlocked);
+  });
+});
+
+describe("when `onUnlocked` is undefined", () => {
+  beforeEach(() => {
+    props.onUnlocked = undefined;
+  });
+
+  it("sets the rendered `SlideToUnlock`'s `onSlide` prop to undefined'", () => {
+    const slideToUnlock = lockScreen().find(SlideToUnlock);
+    expect(slideToUnlock.props().onSlide).not.toBeDefined();
+  });
+});
+
+*/
 
 it('should be able to run tests', () => {
     expect(1 + 2).toEqual(3)
 })
 
+// TEST
+
+// what it renders + what it passes to them
+// props it receives + what does with them
+// state it holds + when updates
+// user input + callbacks
+// context
+// life cycle side effects: what happens when mounted/unmounted?
+
 
 /* 
-
-// also could use
+// EXAMPLES
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App/>, div);
-
-*/
-
-/*
+  
 it('renders welcome message', () => {
   const wrapper = shallow(<App />)
   const welcome = <h2>Welcome to React</h2>
@@ -31,94 +116,53 @@ it('renders welcome message', () => {
   expect(wrapper.contains(welcome)).toEqual(true)
 })
 
-*/
-
-/* Shallow example
-
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import sinon from 'sinon';
-
-import MyComponent from './MyComponent';
-import Foo from './Foo';
-
-describe('<MyComponent />', () => {
-  it('renders three <Foo /> components', () => {
-    const wrapper = shallow(<MyComponent />);
-    expect(wrapper.find(Foo)).to.have.length(3);
-  });
-
-  it('renders an `.icon-star`', () => {
-    const wrapper = shallow(<MyComponent />);
-    expect(wrapper.find('.icon-star')).to.have.length(1);
-  });
-
-  it('renders children when passed in', () => {
-    const wrapper = shallow((
-      <MyComponent>
-        <div className="unique" />
-      </MyComponent>
-    ));
-    expect(wrapper.contains(<div className="unique" />)).to.equal(true);
-  });
-
-  it('simulates click events', () => {
-    const onButtonClick = sinon.spy();
-    const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
-    wrapper.find('button').simulate('click');
-    expect(onButtonClick).to.have.property('callCount', 1);
-  });
+it('renders three <Foo /> components', () => {
+  const wrapper = shallow(<MyComponent />);
+  expect(wrapper.find(Foo)).to.have.length(3);
 });
 
-*/
-
-/* Mount example 
-
-
-    expect(wrapper.contains(<div className="unique" />)).to.equal(true);
-  });
-
-  it('simulates click events', () => {
-    const onButtonClick = sinon.spy();
-    const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
-    wrapper.find('button').simulate('click');
-    expect(onButtonClick).to.have.property('callCount', 1);
-  });
+it('renders an `.icon-star`', () => {
+  const wrapper = shallow(<MyComponent />);
+  expect(wrapper.find('.icon-star')).to.have.length(1);
 });
-Read the full API Documentation
 
-Full DOM Rendering
-import React from 'react';
-import sinon from 'sinon';
-import { expect } from 'chai';
-import { mount } from 'enzyme';
+it('renders children when passed in', () => {
+  const wrapper = shallow((
+    <MyComponent>
+      <div className="unique" />
+    </MyComponent>
+  ));
+  expect(wrapper.contains(<div className="unique" />)).to.equal(true);
+});
 
-import Foo from './Foo';
+it('simulates click events', () => {
+  const onButtonClick = sinon.spy();
+  const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
+  wrapper.find('button').simulate('click');
+  expect(onButtonClick).to.have.property('callCount', 1);
+});
 
-describe('<Foo />', () => {
-  it('allows us to set props', () => {
-    const wrapper = mount(<Foo bar="baz" />);
-    expect(wrapper.props().bar).to.equal('baz');
-    wrapper.setProps({ bar: 'foo' });
-    expect(wrapper.props().bar).to.equal('foo');
-  });
+it('allows us to set props', () => {
+  const wrapper = mount(<Foo bar="baz" />);
+  expect(wrapper.props().bar).to.equal('baz');
+  wrapper.setProps({ bar: 'foo' });
+  expect(wrapper.props().bar).to.equal('foo');
+});
 
-  it('simulates click events', () => {
-    const onButtonClick = sinon.spy();
-    const wrapper = mount((
-      <Foo onButtonClick={onButtonClick} />
-    ));
-    wrapper.find('button').simulate('click');
-    expect(onButtonClick).to.have.property('callCount', 1);
-  });
+it('simulates click events', () => {
+  const onButtonClick = sinon.spy();
+  const wrapper = mount((
+    <Foo onButtonClick={onButtonClick} />
+  ));
+  wrapper.find('button').simulate('click');
+  expect(onButtonClick).to.have.property('callCount', 1);
+});
 
-  it('calls componentDidMount', () => {
-    sinon.spy(Foo.prototype, 'componentDidMount');
-    const wrapper = mount(<Foo />);
-    expect(Foo.prototype.componentDidMount).to.have.property('callCount', 1);
-    Foo.prototype.componentDidMount.restore();
-  });
+it('calls componentDidMount', () => {
+  sinon.spy(Foo.prototype, 'componentDidMount');
+  const wrapper = mount(<Foo />);
+  expect(Foo.prototype.componentDidMount).to.have.property('callCount', 1);
+  Foo.prototype.componentDidMount.restore();
 });
 
 */
