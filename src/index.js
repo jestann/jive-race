1cf4d0e97bc418e6e1c53e731fb94b54839edff1
Jess Bird <jessannbird@gmail.com>
2018-02-28 06:58:58 +0000

    standardizing functional version

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom' // includes { render } from 'react-dom'
-// import { createStore } from 'redux'
 import registerServiceWorker from './tools/registerServiceWorker'
 
 import App from './components/app'
-// import reducer from './store/reducers/reducer'
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/main/main.css'
 
-// let store = createStore(reducer)
 
 const render = () => {
   ReactDOM.render(
-    <App /* store={store} */ />, 
+    <App />, 
     document.getElementById('root')
   )
 }
-
-// store.subscribe(render)
-// render()
 registerServiceWorker()
-
-// can add additional components in div with same store, like <Results store={store}/>
