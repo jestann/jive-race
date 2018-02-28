bd3c1381174c545c9445800f29e9ad9e3df14ae5
Jess Bird <jessannbird@gmail.com>
2018-02-28 06:59:31 +0000

    refactoring testing structure

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,29 +3,3 @@ import Adapter from 'enzyme-adapter-react-15'
 import 'jest-enzyme'
 
 configure({ adapter: new Adapter() })
-
-
-
-/*
-
-// package.json
-"test": "jest --config jest.config.js"
-
-// config.jest.js
-{
-    "moduleNameMapper": {
-        "Clock": "<rootDir>/app/components/Clock.jsx",
-        "CountdownForm": "<rootDir>/app/components/CountdownForm.jsx",
-        "Countdown": "<rootDir>/app/components/Countdown.jsx"
-    }
-}
-
-// to add mocks
-
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  clear: jest.fn()
-};
-global.localStorage = localStorageMock
-*/
